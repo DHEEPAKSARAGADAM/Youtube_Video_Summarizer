09c79beaecf7921428b9d1c162a50705e27810d4
DheepakSaragadam <119062283+DHEEPAKSARAGADAM@users.noreply.github.com>
2023-10-09 19:49:41 +0530

    Runapi.py

--- Runapi.py
+++ Runapi.py
@@ -2,6 +2,5 @@ Fast api http request
 http://127.0.0.1:8000/docs
 
 You can test the API by making POST requests to /summarize/ with a JSON payload containing the YouTube video URL and, optionally, keywords for summarization. The API will respond with the summary text.
-
 This structure provides you with a FastAPI application that tokenizes and preprocesses text, summarizes it, and allows you to provide keywords for summarization.
 
